9fe220fce6b4aa4cb0f856094b5c11bf2a3e47b7
saketrungta <saketrungta00@gmail.com>
2023-03-04 15:46:21 +0530

    Unreal project created and added

---
+++ Source/SteamMultiplayer/SteamMultiplayer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SteamMultiplayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SteamMultiplayer, "SteamMultiplayer" );
